a9d55b82c85faa3e9682bc5b1cf2ed66d770e478
yonguk <lyw3059@gmail.com>
2024-10-27 01:28:15 +0900

    fix: find user location and load map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import Map from "@/components/Map/Map";
+import useUserLocation from "@/hooks/map/useUserLocation";
 
 export default function Home() {
+  const userLocation = useUserLocation();
+
+  // 사용자의 위치가 아직 로딩되지 않았거나 null일 경우
+  if (!userLocation) {
+    return <div>로딩 중...</div>;
+  }
 
   return (
     <div style={{ height: '100vh' }}>
-      <Map center={{ lat: 35.89162826538086, lng: 127.7489013671875 }} zoom={14} />
+      <Map center={userLocation} zoom={14} />
     </div>
   );
-};
+}
